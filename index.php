932289f8def3bbb33443cb0ccdd3a4536f89a183
198cad <198cad@gmail.com>
2018-06-29 08:00:48 +0700

    hapus dah

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(isset($_GET["link"])){
 }else{
 	$link = "";
 }
-// komentar lainya ksdkasdasdasdas
+// hapus dah
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
