a9866328634af54e6071422c2e103daba7887d71
198cad <198cad@gmail.com>
2018-06-29 07:27:06 +0700

    komen ahmed

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if(isset($_GET["link"])){
 }else{
 	$link = "";
 }
+// ini adalah komen ahmed
   //echo $link;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
