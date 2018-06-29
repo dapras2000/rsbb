c1926c83efdda0bb34a39c20c8c75ebfdf2e96d4
198cad <198cad@gmail.com>
2018-06-29 07:36:06 +0700

    komen terbuka

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if(isset($_GET["link"])){
 // ini adalah komen ahmed okde sdadasdasdsa
   //echo $link;
   // CAMPUR
+//   ini adalah komentar terbuka dan lainya
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
